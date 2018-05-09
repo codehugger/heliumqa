4182193d5f3ce8db364f46be93eadea770d1696c
Bjarki Gudlaugsson <bjarkigud@gmail.com>
2018-05-09 15:55:26 +0000

    Javascript cleanup and JSON improvements

--- app/assets/javascripts/scan_protocol_matchers.js
+++ app/assets/javascripts/scan_protocol_matchers.js
@@ -1,21 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-// FIXME: this does not work with many matchers on the screen at the same time
-document.addEventListener('turbolinks:load', () => {
-  var tag_matchers = $('#value_type_matcher_id').html()
-
-  function filterTags() {
-    var scan_header_tag = $('#scan_header_tag_id :selected').text()
-    var options = $(tag_matchers).filter(`optgroup[label="${scan_header_tag}"]`).html()
-    if (options) {
-      $('#value_type_matcher_id').html(options)
-    } else {
-      $('#value_type_matcher_id').empty()
-    }
-  }
-
-  $('#scan_header_tag_id').change(filterTags)
-
-  // filterTags()
-})
--- app/views/scan_protocols/_scan_protocol.json.jbuilder
+++ app/views/scan_protocols/_scan_protocol.json.jbuilder
@@ -1,2 +1,8 @@
 json.extract! scan_protocol, :id, :site_id, :name, :created_at, :updated_at
+json.specifications_file_url scan_protocol.specifications_file_url
+json.analysis_options_file_url scan_protocol.analysis_options_file_url
 json.url scan_protocol_url(scan_protocol, format: :json)
+json.phantom_module_files scan_protocol.phantom_module_files.each do |file|
+  json.file_url file.file_url
+  json.filename file.file.original_filename
+end
